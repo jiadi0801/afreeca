300bf48a9b2d3f95e550c7ba081492dabe6c066e
liujia <liujia30@jd.com>
2017-07-03 21:57:17 +0800

    引入css样式库 spectre

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,71 @@
 <template>
     <div id="app">
         <div>头部 公用的</div>
-        <div class="row">
-            <div class="col-1">
+        <div class="columns">
+            <div class="col-2">
                 <toolbar></toolbar>
                 <div>列表、以树状展示、以平铺展示</div>
             </div>
-            <div class="col-5">
-                <div>名称：接口描述</div>
-                <div>方法 url</div>
-                <div>response选择列表</div>
+            <div class="col-8">
+                <div class="columns">
+                    <span class="col-2" >
+                        <input type="text" placeholder="名称" class="form-input">
+                    </span>
+                    <span class="col-10">
+                        <input type="text" placeholder="描述" class="form-input">
+                    </span>
+                </div>
+                <div class="columns">
+                    <span class="col-2">
+                        <select type="text" placeholder="名称" class="form-select">
+                            <option value="GET">GET</option>
+                            <option value="POST">POST</option>
+                        </select>
+                    </span>
+                    <span class="col-10">
+                        <input type="text" placeholder="URL" class="form-input">
+                    </span>
+                </div>
+                <h4>response选择列表</h4>
                 <div>
-                    <div>
-                        <div>response1名字：描述</div>
-                        <div>response1内容</div>
+                    response1 ,response2
+                </div>
+                <div>
+                    <div class="columns">
+                        <span class="col-2" >
+                            <input type="text" placeholder="Response名称" class="form-input">
+                        </span>
+                        <span class="col-10">
+                            <input type="text" placeholder="描述" class="form-input">
+                        </span>
                     </div>
-                    <div>
-                        <div>response2名字：描述</div>
-                        <div>response2内容</div>
+                    <div class="columns">
+                        <div class="col-6">
+                            <label class="form-label">YAML</label>
+                            <textarea class="form-input"></textarea>
+                        </div>
+                        <div class="col-6">
+                            <label class="form-label">JSON</label>
+                            <textarea class="form-input"></textarea>
+                        </div>
+                    </div>
+                    <div class="columns">
+                        <span class="col-2" >
+                            <input type="text" placeholder="Response2名称" class="form-input">
+                        </span>
+                        <span class="col-10">
+                            <input type="text" placeholder="描述" class="form-input">
+                        </span>
+                    </div>
+                    <div class="columns">
+                        <div class="col-6">
+                            <label class="form-label">YAML</label>
+                            <textarea class="form-input"></textarea>
+                        </div>
+                        <div class="col-6">
+                            <label class="form-label">JSON</label>
+                            <textarea class="form-input"></textarea>
+                        </div>
                     </div>
                 </div>
             </div>
